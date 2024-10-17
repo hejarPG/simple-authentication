2085bfb632ead11796dcdf0a6cf090454677f782
hejarPG <hejar.adak@gmail.com>
2024-10-17 15:09:58 +0330

    Fix db conenction config file directory

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 import "dotenv/config";
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
-import sql from "./db";
+import sql from "./configs/db";
 
 const app = new Hono();
 
-app.get("/", async (c) => {
-  console.log(await sql`SELECT * FROM users`);
+// app.get("/", async (c) => {
+//   console.log(await sql`SELECT * FROM users`);
 
-  return c.text("Hello Hono!");
-});
+//   return c.text("Hello Hono!");
+// });
 
 const port = 3000;
 console.log(`Server is running on port ${port}`);
