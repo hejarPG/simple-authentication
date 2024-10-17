118c6910a499fd67519278613b285f31c58b3b63
hejarPG <hejar.adak@gmail.com>
2024-10-17 15:14:23 +0330

    Add exists func. to users model

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,7 +1,15 @@
+import sql from "../configs/db";
+
 interface User {
   id: number;
   username: string;
   password: string;
 }
 
-const Users = {};
+const Users = {
+  exists: async (username: string): Promise<boolean> => {
+    return (
+      (await sql`select true from users where username=${username}`).count !== 0
+    );
+  },
+};
